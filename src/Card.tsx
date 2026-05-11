4cbe58b9f12c0eb26a7d69a9ba89a5094d9049ab
Kaua <kauaoliveira18062007@gmail.com>
2026-05-11 19:14:16 -0300

    feat: implement Tailwind CSS instead of using pure CSS

--- src/Card.tsx
+++ src/Card.tsx
@@ -3,24 +3,84 @@ interface CardProps{
     title: string;
     price: string;
     benefits: string[];
+    variant: "gold" | "silver" | "bronze";
 }
 
 export function Card(Props: CardProps){
+
+    const styles = {
+        bronze: "border-orange-600 hover:border-orange-500 text-orange-600 hover:shadow-lg w-48 h-48",
+        silver: "border-gray-600 hover:border-gray-500 text-gray-600 hover:shadow-lg w-48 h-48",
+        gold: "border-yellow-600 hover:border-yellow-500 text-yellow-600 hover:shadow-lg w-64 h-64"
+    }
+
+    const buttonStyles = {
+        bronze: "bg-orange-600 hover:bg-orange-500",
+        silver: "bg-gray-600 hover:bg-gray-500",
+        gold: "bg-yellow-600 hover:bg-yellow-500"
+    }
+
     return (
-        <div style={{ 
-            border: '1px solid #ccc', 
-            padding: '20px',
-            borderRadius: '8px',
-            margin: '10px'}}>
+        <div 
+            className={`
+                border 
+                border-gray-300 p-5 
+                rounded-lg m-2.5
+                text-center 
+                shadow-md bg-white
+                ${styles[Props.variant]}
+            `}
+        >
+
+            <div>
+                <h2 
+                className={`
+                    text-xl
+                    font-bold
+                    mb-2
+                `}
+                >{Props.title}</h2>
+            </div>
             
-            <h2>{Props.title}</h2>
-            <p>Price: {Props.price}</p>
-            <ul>
-                {Props.benefits.map((item) => (
-                    <li key={item}>{item}</li>
-                ))}
-            </ul>
-            <button>Subscribe now</button>
+            
+            <div>
+                    <ul 
+                    className="
+                        text-left
+                        list-disc
+                        list-inside
+                        mb-2
+                        space-y-1
+                    "
+                >
+                    {Props.benefits.map((item) => (
+                        <li key={item}
+                            className={`
+                                text-sm
+                            `}
+                        >{item}</li>
+                    ))}
+                </ul>
+            </div>
+            
+            <div>
+                <button 
+                className={`
+                        w-full
+                        ${buttonStyles[Props.variant]}
+                        duration-300
+                        text-white
+                        font-bold
+                        py-2
+                        px-4
+                        border-none
+                        border-yellow-700
+                        rounded
+                        shadow-md
+                `}
+                >{Props.price}</button>
+            </div>
+            
         </div>
 
     );
